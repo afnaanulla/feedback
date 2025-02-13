98cb5079cffad1b92911f4795a087c0ff43b6897
MohammadAfnaan <afnaan31250@gmail.com>
2025-02-13 13:59:49 +0530

    finla commit

--- routes/auth.js
+++ routes/auth.js
@@ -26,16 +26,16 @@ module.exports = router;
 
 
 const express = require('express');
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const generateTokens = require('../utils/generateTokens');
 
 const router = express.Router();
 
-
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
+    
+    // Find user by username
     const user = await User.findOne({ username });
     if (!user) {
         return res.status(400).json({
@@ -43,21 +43,25 @@ router.post('/login', async (req, res) => {
             success: false
         });
     }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
+
+    // Direct password comparison (NO HASHING)
+    if (user.password !== password) {
         return res.status(400).json({
             message: 'Login unsuccessful: Incorrect password',
             success: false
         });
     }
+
+    // Generate JWT tokens
     const tokens = generateTokens(user._id);
+    
     res.json({
         message: 'Logged in successfully!',
         success: true,
         user: {
             userId: user._id,
             username: user.username,
-            password: password,
+            password: user.password,  // *Returning plain-text password (Not recommended)*
             name: user.name
         },
         tokens
